93e562cfe0bad17176ca7a1ea490f510ab61ad89
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2024-11-18 14:13:35 +0000

    Apagar Tópico - Feito

---
+++ topics_app/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Topic
+
+class TopicForm(ModelForm):
+    class Meta:
+        model = Topic
+        fields = ["title", "description"]
+
--- topics_app/views.py
+++ topics_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Topic
 from .forms import TopicForm
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def lista_topicos(request):
@@ -28,10 +29,21 @@ def criacao_topicos(request):
     return render(request, "topics_app/criarTopico.html", {"form": form})
 
 @login_required
-def detail(request, question_id):
+def detalhes_topicos(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
+@login_required
+def apagar_topico(request, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    if request.user != topic.author:
+        return HttpResponse("Não tem permissão para apagar o evento.", status=403)
+    if request.method == "POST":
+        topic.delete()
+        return redirect("/")
+    return render(request, "topics_app/apagarTopico.html", {"topic": topic})
 
+
+ 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
